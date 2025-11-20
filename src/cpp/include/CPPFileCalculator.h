e25157fd350db228303f4419fa91a7fc41073c5e
AbhiSwamiUConn <abhiswami2005@gmail.com>
2025-11-20 00:17:47 -0500

    implemented c++ bindings

---
+++ src/cpp/include/CPPFileCalculator.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Calculator.h"
+#include <string>
+
+class CPPFileCalculator:public Calculator{
+public:
+    CPPFileCalculator();
+    long long sum_file(const std::string& path = "");
+};
